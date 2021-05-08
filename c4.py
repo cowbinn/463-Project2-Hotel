a04c76c6cf37b97f0be81692d85777ae4e9e3ea5
austinhoang47 <42751380+austinhoang47@users.noreply.github.com>
2021-05-07 21:13:31 -0700

    done

--- c4.py
+++ c4.py
@@ -12,9 +12,11 @@ window.title('Housekeeping')
 window.resizable(True, True)
 background_color = 'light gray'
 labelFrame = Frame(window, width=width, height=height)
+conn = sqlite3.connect('hotel.db')
+cur = conn.cursor()
 
 treeView = ttk.Treeview(window, columns=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), show="headings", height=20)
-treeView.pack()
+treeView.pack(side='right')
 treeView.heading(1, text="Room number")
 treeView.heading(2, text="Housekeeper Name")
 treeView.heading(3, text="Room Type")
@@ -29,7 +31,7 @@ treeView.heading(10, text="Electronics")
 treeView.column(1, width=120)
 treeView.column(2, width=120)
 treeView.column(3, width=120)
-treeView.column(4, width=120)
+treeView.column(4, width=150)
 treeView.column(5, width=120)
 treeView.column(5, width=120)
 treeView.column(6, width=120)
@@ -38,29 +40,105 @@ treeView.column(8, width=120)
 treeView.column(9, width=120)
 treeView.column(10, width=120)
 
-instructions_label = Label(labelFrame, text='Select a room and then click any of the options to check off', bg=background_color, fg='black', font=('Arial', 12))
-instructions_label.place(x=160, y=450)
+sql = "SELECT RoomNumber, HousekeeperName, RoomType, RoomStatus, Bathroom, Towels, BedSheets," \
+      "Vacuum, Dusting, Electronics FROM Room"
+data = cur.execute(sql)
+data = cur.fetchall()
+for i in data:
+    treeView.insert('', 'end', values=i)
 
-bathroom_checkOff = Button(window, text="Check off Bathrooms")
+def bathrooms():
+    selected = treeView.focus()
+    temp = treeView.item(selected, 'values')
+    cur.execute("UPDATE Room SET Bathroom='Checked' WHERE RoomNumber=?", (temp[0],))
+    conn.commit()
+    treeView.item(selected, text="", values=(temp[0], temp[1], temp[2], temp[3],
+                                             "Checked", temp[5], temp[6], temp[7], temp[8], temp[9]))
+
+def towels():
+    selected = treeView.focus()
+    temp = treeView.item(selected, 'values')
+    cur.execute("UPDATE Room SET Towels='Checked' WHERE RoomNumber=?", (temp[0],))
+    conn.commit()
+    treeView.item(selected, text="", values=(temp[0], temp[1], temp[2], temp[3],
+                                             temp[4], "Checked", temp[6], temp[7], temp[8], temp[9]))
+def bedsheets():
+    selected = treeView.focus()
+    temp = treeView.item(selected, 'values')
+    cur.execute("UPDATE Room SET BedSheets='Checked' WHERE RoomNumber=?", (temp[0],))
+    conn.commit()
+    treeView.item(selected, text="", values=(temp[0], temp[1], temp[2], temp[3],
+                                             temp[4], temp[5], "Checked", temp[7], temp[8], temp[9]))
+
+def vacuum():
+    selected = treeView.focus()
+    temp = treeView.item(selected, 'values')
+    cur.execute("UPDATE Room SET Vacuum='Checked' WHERE RoomNumber=?", (temp[0],))
+    conn.commit()
+    treeView.item(selected, text="", values=(temp[0], temp[1], temp[2], temp[3],
+                                             temp[4], temp[5], temp[6], "Checked", temp[8], temp[9]))
+
+def dusting():
+    selected = treeView.focus()
+    temp = treeView.item(selected, 'values')
+    cur.execute("UPDATE Room SET Dusting='Checked' WHERE RoomNumber=?", (temp[0],))
+    conn.commit()
+    treeView.item(selected, text="", values=(temp[0], temp[1], temp[2], temp[3],
+                                             temp[4], temp[5], temp[6], temp[7], "Checked", temp[9]))
+
+def electronics():
+    selected = treeView.focus()
+    temp = treeView.item(selected, 'values')
+    cur.execute("UPDATE Room SET Electronics='Checked' WHERE RoomNumber=?", (temp[0],))
+    conn.commit()
+    treeView.item(selected, text="", values=(temp[0], temp[1], temp[2], temp[3],
+                                             temp[4], temp[5], temp[6], temp[7], temp[8], "Checked"))
+
+def checkAll():
+    selected = treeView.focus()
+    temp = treeView.item(selected, 'values')
+    cur.execute("UPDATE Room SET RoomStatus = 'Available', Bathroom='Checked', Towels='Checked', BedSheets='Checked',"
+                "Vacuum='Checked', Dusting='Checked', Electronics='Checked' WHERE RoomNumber=?", (temp[0],))
+    conn.commit()
+    treeView.item(selected, text="", values=(temp[0], temp[1], temp[2], "Available",
+                                             "Checked", "Checked", "Checked", "Checked", "Checked", "Checked"))
+
+def unavailable_maintenance():
+    selected = treeView.focus()
+    temp = treeView.item(selected, 'values')
+    cur.execute("UPDATE Room SET RoomStatus='Unavailable/Maintenance', Bathroom=NULL, Towels=NULL, BedSheets=NULL,"
+                "Vacuum=NULL, Dusting=NULL, Electronics=NULL WHERE RoomNumber=?", (temp[0],))
+    conn.commit()
+    treeView.item(selected, text="", values=(temp[0], temp[1], temp[2], "Unavailable/Maintenance",
+                                             "None", "None", "None", "None", "None", "None"))
+
+
+instructions_label = Label(labelFrame, text='Select a room and then click any of the options to check off', bg=background_color, fg='black', font=('Arial', 12))
+instructions_label.place(x=50, y=5)
+
+bathroom_checkOff = Button(window, text="Check off Bathrooms", command=bathrooms)
 bathroom_checkOff.place(x=70, y=30)
 
-towels_checkOff = Button(window, text="Check off Towels")
+towels_checkOff = Button(window, text="Check off Towels", command=towels)
 towels_checkOff.place(x=70, y=60)
 
-bedsheets_checkOff = Button(window, text="Check off Bed Sheets")
+bedsheets_checkOff = Button(window, text="Check off Bed Sheets", command=bedsheets)
 bedsheets_checkOff.place(x=70, y=90)
 
-vacuum_checkOff = Button(window, text="Check off Vacuum")
+vacuum_checkOff = Button(window, text="Check off Vacuum", command=vacuum)
 vacuum_checkOff.place(x=70, y=120)
 
-dusting_checkOff = Button(window, text="Check off Dusting")
+dusting_checkOff = Button(window, text="Check off Dusting", command=dusting)
 dusting_checkOff.place(x=70, y=150)
 
-electronics_checkOff = Button(window, text="Check off Electronics")
+electronics_checkOff = Button(window, text="Check off Electronics", command=electronics)
 electronics_checkOff.place(x=70, y=180)
 
-all_checkOff = Button(window, text="Check off All")
+all_checkOff = Button(window, text="Check off All/Make Available", command=checkAll)
 all_checkOff.place(x=70, y=210)
+
+unavailable_changeStatus = Button(window, text="Unavailable/Maintenance", command=unavailable_maintenance)
+unavailable_changeStatus.place(x=70, y=240)
 
 labelFrame.place(x=0, y=0)
 window.mainloop()
