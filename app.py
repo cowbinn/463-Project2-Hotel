d374f1f4be90cda71128b0daa93fdc4307da5e39
cnewen <xhl.nguyen@gmail.com>
2021-05-07 20:51:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,28 @@ from Roomtype import roomType
 
 root = Tk()
 root.title("Main Screen")
-root.geometry("800x600")
+root.geometry("600x300")
 
-logo_label = Label(root, text="Antares Hotel Management Company", font=('Arial', 24)).place(x=120,y=10)
+logo_label = Label(root, text="Antares Hotel Management Company", font=('Arial', 18)).place(x=100,y=30)
 
 room_type_button = Button(root, text="C1 - Room Status", command=roomType)
-room_type_button.place(x=280, y=100)
+room_type_button.place(x=100, y=120)
+
+room_list_button = Button(root, text="C2 - Room List", command=print('roomlistfunctionhere'))
+room_list_button.place(x=400, y=120)
+
+reservation_button = Button(root, text="C3 - Reservation", command=print('reservationfunctionhere'))
+reservation_button.place(x=100, y=170)
+
+housekeeping_button = Button(root, text="C4 - Housekeeping", command=print('housekeepingfunctionhere'))
+housekeeping_button.place(x=400, y=170)
+
 
 search_menu_button = Button(root, text="C7 - Search for Guest", command=search_menu)
-search_menu_button.place(x=280, y=150)
+search_menu_button.place(x=100, y=220)
 
 daily_report_button = Button(root, text="C8 - Daily Report", command=daily_report_menu)
-daily_report_button.place(x=280, y=200)
+daily_report_button.place(x=400, y=220)
 
 
 root.mainloop()
